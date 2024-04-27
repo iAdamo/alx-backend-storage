e49e852f68912f4d7da2842942f8e891d6f5b1f8
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2024-04-27 17:45:25 +0100

    Writing strings to Redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Module for redis exercise
+"""
+
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """Cache class
+    """
+
+    def __init__(self):
+        """Constructor method
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Store data in redis db
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
